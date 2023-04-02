20fc3057490b6b52a32f7db86a14e376eeb40a50
Ichlas02 <ichlasulhasanat.205025@mhs.its.ac.id>
2023-04-02 14:07:47 +0700

    fixed paddle physics

--- Scripts/Paddle.cs
+++ Scripts/Paddle.cs
@@ -8,9 +8,44 @@ using Unity.MLAgents.Sensors;
 
 public class Paddle : Agent
 {
-    [SerializeField] private Transform targetTransform;
+    [SerializeField] HingeJoint LeftFlipper;
+    [SerializeField] HingeJoint RightFlipper;
+
+    public int FlipperMotorVelocity;
+    public int FlipperMotorForce;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RightFlipper.motor = RotateFlipper(FlipperMotorVelocity, FlipperMotorForce);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+        RightFlipper.motor = RotateFlipper(-FlipperMotorVelocity, FlipperMotorForce);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            LeftFlipper.motor = RotateFlipper(-FlipperMotorVelocity, FlipperMotorForce);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+        LeftFlipper.motor = RotateFlipper(FlipperMotorVelocity, FlipperMotorForce);
+        }
+    }
+    JointMotor RotateFlipper(float velocity, float force)
+    {
+        JointMotor jointMotor = new JointMotor();
+        jointMotor.force = force;
+        jointMotor.targetVelocity = velocity;
+        return jointMotor;
+    }
+
     public override void OnActionReceived(ActionBuffers actions)
     {
         Debug.Log(actions.DiscreteActions[0]);
-    }
+	}
 }
